38157cccd39070ba0b17efb1532e3fbd7e1c6df0
Jonathan Katz <jonathan.a.katz@gmail.com>
2016-09-29 17:18:26 -0700

    add base.js

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: 'AIzaSyCNNDQE9YW1ga5SB_2DJc-1tgwf_KKkr9s',
+  authDomain: 'seafood-menu-b690a.firebaseapp.com',
+  databaseURL: 'https://seafood-menu-b690a.firebaseio.com'
+});
+
+export default base;
